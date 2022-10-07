9f36dffb8175985449edb136e1dff5da1e40b8c3
mattsershaw <masato.sasho@gmail.com>
2022-10-07 23:51:38 +0900

    ヘッダー作成

--- public/js/app.js
+++ public/js/app.js
@@ -37362,8 +37362,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/Masato/Desktop/qiita/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/Masato/Desktop/qiita/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/Masato/prog/apps/qiita/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/Masato/prog/apps/qiita/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
