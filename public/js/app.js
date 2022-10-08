a8f0b454b7b7d7213d528fae591d02b09176eade
mattsershaw <masato.sasho@gmail.com>
2022-10-08 22:52:28 +0900

    mdをhtmlへ変換

--- public/js/app.js
+++ public/js/app.js
@@ -52453,7 +52453,11 @@ $(function () {
   $('#markdown_editor_textarea').keyup(function () {
     var html = Object(marked__WEBPACK_IMPORTED_MODULE_0__["marked"])(getHtml($(this).val()));
     $('#markdown_preview').html(html);
-  }); // 比較演算子が &lt; 等になるので置換
+  }); // 個別の記事画面のマークダウンをHTMLに変換する
+
+  var target = $('.item-body');
+  var html = Object(marked__WEBPACK_IMPORTED_MODULE_0__["marked"])(getHtml(target.html()));
+  $('.item-body').html(html); // 比較演算子が &lt; 等になるので置換
 
   function getHtml(html) {
     html = html.replace(/&lt;/g, '<');
