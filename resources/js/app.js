79889fbe9dc6a7f019063077e6a20d49c934bce3
mattsershaw <masato.sasho@gmail.com>
2022-10-08 15:27:36 +0900

    マークダウンの導入

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,25 +8,26 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
+import {marked} from 'marked';
 
-// const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
+// マークダウンをプレビュー画面に表示する
+$(function() {
+	marked.setOptions({
+		langPrefix: '',
+		breaks : true,
+		sanitize: true,
+	});
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+	$('#markdown_editor_textarea').keyup(function() {
+		var html = marked(getHtml($(this).val()));
+		$('#markdown_preview').html(html);
+	});
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-const app = new Vue({
-    el: '#app',
+	// 比較演算子が &lt; 等になるので置換
+	function getHtml(html) {
+		html = html.replace(/&lt;/g, '<');
+		html = html.replace(/&gt;/g, '>');
+		html = html.replace(/&amp;/g, '&');
+		return html;
+	}
 });
